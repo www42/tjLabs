3e58fe23a911b77a117b7a4a8ccd9c2a1097bbb7
Thomas Jaekel <Wilhelm.Wien@outlook.de>
2017-12-09 22:19:31 +0100

    set-content

---
+++ SetComputerNameInAnswerFile.ps1
@@ -0,0 +1,3 @@
+﻿$File = "C:\temp\CopyProfile_and_OOBE_and_Computername.xml"
+
+(Get-Content $File).Replace('<ComputerName>MyComputer</ComputerName>','<ComputerName>foo</ComputerName>') | Set-Content $File
