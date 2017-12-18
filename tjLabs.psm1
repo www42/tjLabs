c5473a8e5da9172d0934a48f58494cef2d57d106
Willi Wien <Wilhelm.Wien@outlook.de>
2017-12-18 21:45:05 +0100

    Correct path to answer file

--- tjLabs.psm1
+++ tjLabs.psm1
@@ -373,7 +373,7 @@ function New-LabVmDifferencing {
      New-VHD -Path $VhdPath -Differencing -ParentPath $BaseVhd -SizeBytes 127GB | Out-Null
      Mount-VHD -Path $VhdPath
      $DriveLetter = Get-DiskImage -ImagePath $VhdPath | Get-Disk | Get-Partition | Get-Volume | ? FileSystemLabel -NE "Recovery" | % DriveLetter
-     $AnswerFile = $DriveLetter + ":\Windows\System32\Sysprep\CopyProfile_and_OOBE_and_Computername.xml"
+     $AnswerFile = $DriveLetter -join ":\Windows\Panther\unattend.xml"
      (Get-Content $AnswerFile).Replace('<ComputerName>MyComputer</ComputerName>','<ComputerName>' + $VmComp + '</ComputerName>') | Set-Content $AnswerFile
      Dismount-VHD -Path $VhdPath
      New-VM -Name $VmName -Generation 2 -Path $Dir -VHDPath $VhdPath -MemoryStartupBytes $Mem -SwitchName $Switch -Version $Version | Out-Null
