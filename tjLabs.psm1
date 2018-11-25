1a54e3840e6e5a2eb5be5f0fd9e5fa8f8889874c
Thomas Jaekel <Wilhelm.Wien@outlook.de>
2018-11-25 18:28:56 +0100

    fixed ConvertTo-ComputerName

--- tjLabs.psm1
+++ tjLabs.psm1
@@ -14,11 +14,24 @@
   }
 }
 function ConvertTo-ComputerName {
-  [CmdletBinding()]Param(
-  [Parameter(Mandatory=$true,Position=1)][string]$VmName
-  )
-  $ComputerName = $VmName.split("-",2)[1]
-  Write-Output $ComputerName
+<#
+.Synopsis
+   Converts VmName to ComuterName by trimming off the LAB string
+.DESCRIPTION
+   Within LAB environment this function converts a VmName into the corresponding ComputerName by trimming off the LAB string.
+   E. g. "20740B-DC1" becomes "DC1" within 20740B Lab.
+.EXAMPLE
+   ConvertTo-ComputerName  20740B-DC1  20740B
+#>
+Param (
+  [Parameter(Mandatory=$true,Position=0)]
+  [string]$VmName,
+
+  [Parameter(Mandatory=$true,Position=1)]
+  [string]$Lab
+)
+$ComputerName = $VmName.TrimStart($Lab)
+Write-Output $ComputerName
 }
 function ConvertTo-VmName {
     [CmdletBinding()]Param(
